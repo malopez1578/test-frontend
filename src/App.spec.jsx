b6cbc0ecba7008b2cc4a3ec543ced7f4ba1044b4
malopez1578 <malopez1578@gmail.com>
2021-08-13 18:39:19 -0500

    update(App.test): remove test

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,17 +1,12 @@
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import App from '../src/App';
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from '../src/App'
 
-
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
 
 describe('App.jsx', () => {
-
   it('renders', () => {
     const wrapper = shallow(<App />)
-    const p = wrapper.find('p')
-
-    expect(p.text()).equal('HMR updates')
+    expect(sum(1, 2)).toBe(3)
   })
-
 })
